55940753c540fd8a653838bc435d920ec9e489bc
Shawn Houston <houston.shawn@gmail.com>
2021-07-14 12:49:31 -0400

    tweaking 3

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
     $fileExtension = strtolower(end($fileNameCmps));
  
     // sanitize file-name
-    $newFileName = md5(time()) . '-' . preg_replace("/[^a-z0-9\.]/", "", strtolower($fileName)) . '.' . $fileExtension;
+    $newFileName = md5(time()) . '-' . preg_replace("/[^a-z0-9\.]/", "", strtolower($fileName));
  
     // check if file has one of the following extensions
     $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc', 'pdf');
